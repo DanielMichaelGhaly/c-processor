0afff60cef9b42a39f1e90327082cee527641152
Daniel M. Hennawi <151990013+DanielMichaelGhaly@users.noreply.github.com>
2025-05-19 15:44:51 +0200

    implemented the functionality of the queue data structure

--- queue.h
+++ queue.h
@@ -1,21 +1,21 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
-#include <stdbool.h>
-
 #define MAX_SIZE 10
 
 typedef struct Queue{
     int items[MAX_SIZE];
     int front;
     int back;
-};
+}Queue;
 
 void initQueue(Queue *q);
 
-bool isEmpty(Queue *q);
+// if return value is 1, the queue is empty, else 0
+int isEmpty(Queue *q);
 
-bool isFull(Queue *q);
+//if return value is 1, the queue is full, else 0
+int isFull(Queue *q);
 
 void enqueue(Queue *q, int value);
 
