2f40c146d57f760880a95dc83963a1cb47c528ae
Daniel M. Hennawi <151990013+DanielMichaelGhaly@users.noreply.github.com>
2025-05-19 17:39:25 +0200

    added a feature to return an array from the execution method to be read correctly to the following stages

--- main.c
+++ main.c
@@ -3,7 +3,10 @@
 
 //ALU[5], branch, memory read, memory write, register write, memory to register, shift[2]
 //if for example ALU[0] = 1 then ADD for example, if branch is 1 then will branch, if shift[1] == 1 then shift right
-void execute(int ALU[], int branch, int memoryRead, int memoryWrite, int registerWrite, int memoryToRegister, int shft[] , int R1, int R2, int R3){
+int* execute(int ALU[], int branch, int memoryRead, int memoryWrite, int registerWrite, 
+    int shft[] , int R1, int R2, int R3){
+    int execution_Result[3] = {0};
+    int answer = 0;
     for(int i = 0; i<5; i++){
         if(ALU[i] == 1){
             char * instruction = "";
@@ -15,25 +18,23 @@ void execute(int ALU[], int branch, int memoryRead, int memoryWrite, int registe
                 case 3: instruction = "AND";break;
                 case 4: instruction = "XORI";break;
             }
-            alu(instruction, R1, R2, R3);
+            answer = alu(instruction, R1, R2, R3);
             return;
         }
     }
     if(branch == 1){
-
         return;
     }
-    if(memoryRead == 1){
-            
+    //in memory stage
+    if(memoryRead == 1){       
         return;
     }
+    //in memory stage
     if(memoryWrite == 1){
         return;
     }
+    //in write back stage
     if(registerWrite == 1){
-        return;
-    }
-    if(memoryToRegister == 1){
         return;
     }
     for(int i = 0; i<2; i++){
@@ -44,20 +45,37 @@ void execute(int ALU[], int branch, int memoryRead, int memoryWrite, int registe
                 case 0: instruction = "LSL";break;
                 case 1: instruction = "LSR";break;
             }
-            shift(instruction, R1, R2, R3);
+            answer = shift(instruction, R1, R2, R3);
             return;
         }
     }
 }
 
-void alu(char * instruction, int R1, int R2, int R3)
+int alu(char * instruction, int R1, int R2, int R3)
 {
-
+    if (strcmp(instruction, "ADD") == 0) {
+        R1 = R2 + R3;
+    } else if (strcmp(instruction, "SUB") == 0) {
+        R1 = R2 - R3;
+    } else if (strcmp(instruction, "MUL") == 0) {
+        R1 = R2 * R3;
+    } else if (strcmp(instruction, "AND") == 0) {
+        R1 = R2 & R3;
+    } else if (strcmp(instruction, "XORI") == 0) {
+        R1 = R2 ^ R3;
+    }
+    return R1;
 }
 
-void shift(char * instruction, int R1, int R2, int R3)
+int shift(char * instruction, int R1, int R2, int R3)
 {
-
+    if(strcmp(instruction, "LSL")==0){
+        R1 = R2 << R3;
+    }
+    else if(strcmp(instruction, "LSR")==0){
+        R1 = R2 >> R3;
+    }
+    return R1;
 }
 
 int main()
